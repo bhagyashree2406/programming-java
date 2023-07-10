9017f391f380022c56078864fd7e5e3c30acf918
Admin <bhagyashree6596@gmail.com>
2023-07-10 20:53:53 +0530

    Program to Add Two Integers

---
+++ Programming/src/ClassMain.java
@@ -0,0 +1,13 @@
+public class ClassMain {
+
+    public static void main(String[] args) {
+
+        int first = 10;
+        int second = 20;
+
+        //add to numbers
+
+        int sum = first + second;
+        System.out.println (first + "  +  "  +    second   +   "  =  "  +  sum);
+    }
+}
